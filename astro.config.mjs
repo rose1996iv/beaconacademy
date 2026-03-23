aa5894cd24c544e5a5ad48d259e2ab8bd689ebe9
Joseph <robinsonjoseph.va@gmail.comclear>
2026-03-24 05:27:51 +0530

    feat: implement pagefind global search and scroll progress bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rose1996iv.github.io',
   base: '/beaconacademy',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), pagefind()],
 
   vite: {
     plugins: [tailwindcss()],
